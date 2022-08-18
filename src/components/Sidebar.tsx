87ee948e1cbfa075ff231706c0b7c99e58df96f3
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2022-08-17 21:13:23 -0300

    Configurate Dashboar with Apex Charts

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
-import { RiDashboardLine } from "react-icons/ri";
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
 
 export function Sidebar() {
     return (
@@ -10,7 +10,24 @@ export function Sidebar() {
                     <Stack spacing="4" mt="8" align="stretch">
                         <Link display="flex" >
                             <Icon as={RiDashboardLine} fontSize="20"/>
-                            <Text ml="3">Dashboard</Text>
+                            <Text ml="4" fontWeight="medium">Dashboard</Text>
+                        </Link>
+                        <Link display="flex" >
+                            <Icon as={RiContactsLine} fontSize="20"/>
+                            <Text ml="4" fontWeight="medium">Usuários</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+                <Box>
+                    <Text fontWeight="bold" color="gray.400" fontSize="small">AUTOMAÇÃO</Text>
+                    <Stack spacing="4" mt="8" align="stretch">
+                        <Link display="flex" >
+                            <Icon as={RiInputMethodLine} fontSize="20"/>
+                            <Text ml="4" fontWeight="medium">Formulários</Text>
+                        </Link>
+                        <Link display="flex" >
+                            <Icon as={RiGitMergeLine} fontSize="20"/>
+                            <Text ml="4" fontWeight="medium">Automação</Text>
                         </Link>
                     </Stack>
                 </Box>
